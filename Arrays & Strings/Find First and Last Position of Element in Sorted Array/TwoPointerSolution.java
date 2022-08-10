065bd483dba57c1cecdded95f42b8fcbc8af2093
Himanshu Kumar Gupta <himanshukumargupta135@gmail.com>
2022-08-10 12:07:54 +0530

    Two Pointer Solution.
    https://leetcode.com/problems/find-first-and-last-position-of-element-in-sorted-array/

---
+++ Arrays & Strings/Find First and Last Position of Element in Sorted Array/TwoPointerSolution.java
@@ -0,0 +1,29 @@
+class Solution {
+    public int[] searchRange(int[] nums, int target) {
+        //Taking two pointer left & right
+        int left=0,right=nums.length-1,lIndex=-1,rIndex=-1;
+        while(left<=right) { 
+            
+            //If target not found from left
+            if(lIndex==-1) {
+                //If target found at left
+                if(target==nums[left])
+                    lIndex=left--;  //subtracting left as it will increment next, Consider [0,-1]   
+                left++;
+            }
+            
+            if(rIndex==-1) {
+                if(target==nums[right])
+                    rIndex=right++;         
+                right--;
+            }
+            
+            //Exit if both target found
+            if(lIndex!=-1 && rIndex!=-1) 
+                break;
+        }
+        int output[]={lIndex,rIndex};
+        
+        return output;
+    }
+}
