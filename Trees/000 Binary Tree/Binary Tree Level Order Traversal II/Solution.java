f80c5d77ed8123deb049637ae565f230cc611116
Himanshu Kumar Gupta <himanshukumargupta135@gmail.com>
2022-08-23 01:15:19 +0530

    Binary Tree Level Order Traversal II
    https://leetcode.com/problems/binary-tree-level-order-traversal-ii/

---
+++ Trees/000 Binary Tree/Binary Tree Level Order Traversal II/Solution.java
@@ -0,0 +1,58 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public List<List<Integer>> levelOrderBottom(TreeNode root) {
+        List<List<Integer>> res = new ArrayList<>();
+        
+        //Corner case
+        if(root==null)
+            return res;
+        
+        //Performing level Order Traversal
+        Queue<TreeNode> Q= new LinkedList<>();
+        Q.add(root);
+        Q.add(null);
+        
+        List<Integer> level = new ArrayList<>();
+        while(!Q.isEmpty()) {
+            TreeNode temp = Q.poll();
+            
+            if(temp==null) {
+                List<Integer> tempList = new ArrayList<>();
+                tempList.addAll(level);
+                level.clear();
+                res.add(tempList);
+                
+                if(!Q.isEmpty())
+                    Q.add(null);
+            }
+            else {
+                level.add(temp.val);
+                
+                if(temp.left!=null) 
+                    Q.add(temp.left);
+                
+                if(temp.right!=null)
+                    Q.add(temp.right);
+            }
+        }
+        
+        // Reversing the list at last        
+        Collections.reverse(res);
+        
+        return res;
+    }
+}
