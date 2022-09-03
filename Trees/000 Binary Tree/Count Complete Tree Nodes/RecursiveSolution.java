48e3c2cdd4eac4ba67fd3678ee34a1167b614c4f
Himanshu Kumar Gupta <himanshukumargupta135@gmail.com>
2022-09-04 00:12:12 +0530

    O(n) - Recursive solution to count no of nodes
    https://leetcode.com/problems/count-complete-tree-nodes/

---
+++ Trees/000 Binary Tree/Count Complete Tree Nodes/RecursiveSolution.java
@@ -0,0 +1,29 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public int countNodes(TreeNode root) {
+        if(root==null)
+            return 0;
+        
+        //Count elements in left sub tree
+        int left=countNodes(root.left);
+        //Count elements in right sub tree
+        int right=countNodes(root.right);
+        
+        //count = left+right+root
+        return left+right+1;
+    }
+}
