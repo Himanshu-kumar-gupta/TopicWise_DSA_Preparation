0ad59f63d11565b7dd04521a281e65824ba5aec1
Himanshu Kumar Gupta <himanshukumargupta135@gmail.com>
2022-09-13 01:00:14 +0530

    O(n) - Iterative sol | Two pointers
    https://leetcode.com/problems/middle-of-the-linked-list/

---
+++ Linked List/Middle of the Linked List/IterativeSolution.java
@@ -0,0 +1,21 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+    public ListNode middleNode(ListNode head) {
+        ListNode fast=head;
+        while(fast!=null && fast.next!=null) {
+            head=head.next;
+            fast=fast.next.next;    
+        }
+        
+        return head;
+    }
+}
